081057252d4c73a7a16d2affa24eda1834fa6a73
MohammedMaher177 <anahammo777@gmail.com>
2023-06-24 11:31:32 +0300

    initial

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: index.js
+   Version: I
+   Creation: 02/06/2023
+   Last modification: 03/06/2023
+*/
+
+// Logo
+import bl33hIcon from './logo/bl33hIcon.png';
+
+// Technologies
+import htmlIcon from './tech/html.png';
+import cssIcon from './tech/css.png';
+import jsIcon from './tech/javascript.png';
+import reactIcon from './tech/reactjs.png';
+import awsIcon from './tech/aws.png';
+import javaIcon from './tech/java.png';
+import gitIcon from './tech/git.png';
+import githubIcon from './tech/github.png';
+import figmaIcon from './tech/figma.png';
+import MySQL from './tech/MySQL.png';
+import eslintIcon from './tech/eslint.png';
+import viteIcon from './tech/vite.svg';
+import pyIcon from './tech/py.png';
+import nodeIcon from './tech/node.webp';
+import neoIcon from './tech/neo.png';
+import raspIcon from './tech/raspi.png';
+import close from './close.svg';
+import menu from './menu.svg';
+import next from './tech/next.png';
+import bootstrap from './tech/bootstrap.png';
+import REDUX from './tech/REDUX.png';
+
+// Projects
+import weatherApp from './projects/weather.png';
+import freeGames from './projects/free-games.png';
+import freshCart from './projects/fresh.png';
+
+// Avatar
+import avatar from './avatar/avatar.png';
+
+export {
+  menu,
+  close,
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  eslintIcon,
+  figmaIcon,
+  MySQL,
+  nodeIcon,
+  pyIcon,
+  viteIcon,
+  neoIcon,
+  raspIcon,
+  weatherApp,
+  freeGames,
+  freshCart,
+  avatar,
+  bl33hIcon,
+  next,
+  bootstrap,
+  REDUX
+};
